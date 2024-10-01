e26bc08572089010c7b36ef2aafd516182f33cc0
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-01 16:26:14 +0200

    update css

--- src/App.vue
+++ src/App.vue
@@ -31,40 +31,40 @@ nav a {
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  nav a.router-link-exact-active {
+    color: var(--color-text);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  nav a:first-of-type {
+    border: 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+
+    nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
+
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
   }
 }
 </style>
