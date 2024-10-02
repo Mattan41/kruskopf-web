b5f99b678edc86443c74debaef27d1d56041e463
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-02 08:47:45 +0200

    update css

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="under-construction flex flex-col items-center justify-start h-auto text-center p-8">
     <h1 class="font-medium text-4xl relative -top-2">{{ msg }}</h1>
-    <h3 class="text-xl italic whitespace-nowrap animate-bouncing-banner">This page is currently under construction.</h3>
+    <h3 class="italic whitespace-nowrap animate-bouncing-banner justify-center text-center">This page is currently <br> under construction.</h3>
   </div>
 </template>
 
@@ -23,7 +23,7 @@ defineProps({
 @media (min-width: 1024px) {
   .under-construction h1,
   .under-construction h3 {
-    text-align: left;
+    text-align: center;
   }
 }
 </style>
